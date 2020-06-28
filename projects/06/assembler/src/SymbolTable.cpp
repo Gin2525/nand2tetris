71f897101912d851fa2bb5b165681888ec6abd58
Gin2525 <gin2798@gmail.com>
2020-06-28 20:46:04 +0900

    declared symbol

--- projects/06/assembler/src/SymbolTable.cpp
+++ projects/06/assembler/src/SymbolTable.cpp
@@ -1,16 +1,30 @@
 #include "SymbolTable.hpp"
-#include <string>
 #include <map>
+#include <string>
 
-void SymbolTable::addEntry(std::string symbol, int address){
-    this->symbolTable[symbol]=address;
+SymbolTable::SymbolTable() {
+    this->symbolTable["SP"] = this->SP_ADDRESS;
+    this->symbolTable["LCL"] = this->LCL_ADDRESS;
+    this->symbolTable["ARG"] = this->ARG_ADDRESS;
+    this->symbolTable["THIS"] = this->THIS_ADDERSS;
+    this->symbolTable["THAT"] = this->THAT_ADDRESS;
+
+    for(int r = 0; r <= this->R_MAX_NUMBER; r++)
+        this->symbolTable["R" + r] = r;
+
+    this->symbolTable["SCREEN"] = this->SCREEN_ADDRESS;
+    this->symbolTable["KBD"] = this->KBD_ADDRESS;
 }
 
-bool SymbolTable::contains(std::string symbol){
+void SymbolTable::addEntry(std::string symbol, int address) {
+    this->symbolTable[symbol] = address;
+}
+
+bool SymbolTable::contains(std::string symbol) {
     auto ite = this->symbolTable.find(symbol);
     return ite != this->symbolTable.end();
 }
 
-int SymbolTable::getAddress(std::string symbol){
+int SymbolTable::getAddress(std::string symbol) {
     return this->symbolTable[symbol];
 }
--- projects/06/assembler/src/SymbolTable.hpp
+++ projects/06/assembler/src/SymbolTable.hpp
@@ -4,8 +4,17 @@
 class SymbolTable {
   private:
     std::map<std::string, long> symbolTable;
+    const int SP_ADDRESS = 0;
+    const int LCL_ADDRESS = 1;
+    const int ARG_ADDRESS = 2;
+    const int THIS_ADDERSS = 3;
+    const int THAT_ADDRESS = 4;
+    const int R_MAX_NUMBER = 15;
+    const int SCREEN_ADDRESS = 16384;
+    const int KBD_ADDRESS = 24576;
 
   public:
+    SymbolTable();
     void addEntry(std::string, int);
     bool contains(std::string);
     int getAddress(std::string);
