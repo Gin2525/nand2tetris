d929745c6de3443175a786bf410617f2414c6b87
Gin2525 <gin2798@gmail.com>
2020-06-28 19:11:32 +0900

    rename function and add a constructor

--- projects/06/assembler/src/SymbolTable.hpp
+++ projects/06/assembler/src/SymbolTable.hpp
@@ -6,7 +6,8 @@ class SymbolTable {
     std::map<std::string, long> symbolTable;
 
   public:
+    SymbolTable();
     void addEntry(std::string, int);
     bool contains(std::string);
-    int getContains(std::string);
+    int getAddress(std::string);
 };
