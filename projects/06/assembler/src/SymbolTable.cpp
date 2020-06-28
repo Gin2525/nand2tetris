b93a6dab3cb14c54a96f90b10a89c8b3bb542373
Gin2525 <gin2798@gmail.com>
2020-06-28 19:01:15 +0900

    declared SymbolTable.cpp

--- projects/06/assembler/src/SymbolTable.cpp
+++ projects/06/assembler/src/SymbolTable.cpp
@@ -1,3 +1,12 @@
-#include <string>
 #include <map>
+#include <string>
 
+class SymbolTable {
+  private:
+    std::map<std::string, long> symbolTable;
+
+  public:
+    void addEntry(std::string, int);
+    bool contains(std::string);
+    int getContains(std::string);
+};
