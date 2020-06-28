591af12970ad684326b2eec85e8b22c13f8a608b
Gin2525 <gin2798@gmail.com>
2020-06-28 20:01:44 +0900

    implemented contains in SymbolTable

--- projects/06/assembler/src/SymbolTable.cpp
+++ projects/06/assembler/src/SymbolTable.cpp
@@ -6,6 +6,11 @@ void SymbolTable::addEntry(std::string symbol, int address){
     this->symbolTable[symbol]=address;
 }
 
+bool SymbolTable::contains(std::string symbol){
+    auto ite = this->symbolTable.find(symbol);
+    return ite != this->symbolTable.end();
+}
+
 int SymbolTable::getAddress(std::string symbol){
     return this->symbolTable[symbol];
 }
--- projects/06/assembler/test/TestSymbolTable.cpp
+++ projects/06/assembler/test/TestSymbolTable.cpp
@@ -4,15 +4,29 @@
 
 using namespace std;
 
+void testFailured(std::string testTitle) {
+    cout << "test failured." << endl;
+    cout << "test title : " << testTitle << endl;
+    exit(1);
+}
+
 int main() {
     SymbolTable *table = new SymbolTable();
+    string dummySymbol0 = "yyy";
     string symbol0 = "xxx";
     int address0 = 123;
     table->addEntry(symbol0, address0);
     int actAdd0 = table->getAddress(symbol0);
-    if(actAdd0 != address0) {
-        cout << "test failured" << endl;
-        exit(1);
-    }
+
+    if(actAdd0 != address0)
+        testFailured("whether getting the address?");
+
+    if(table->contains(dummySymbol0))
+        testFailured("throw a dummy address");
+
+    if(!table->contains(symbol0))
+        testFailured("throw a collect adderss");
+
+    cout << "test sucessed" << endl;
     return 0;
 }
