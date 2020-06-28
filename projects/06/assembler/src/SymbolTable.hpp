0e0dabf77b9d636d4dc724bacd2ca6da697aef2d
Gin2525 <gin2798@gmail.com>
2020-06-28 19:31:33 +0900

    Makefile remove constructor in hpp

--- projects/06/assembler/src/SymbolTable.hpp
+++ projects/06/assembler/src/SymbolTable.hpp
@@ -6,7 +6,6 @@ class SymbolTable {
     std::map<std::string, long> symbolTable;
 
   public:
-    SymbolTable();
     void addEntry(std::string, int);
     bool contains(std::string);
     int getAddress(std::string);
